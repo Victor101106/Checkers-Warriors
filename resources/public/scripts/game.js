8f1ea8e711f4d8a66762593e102b7250271695b0
Victor101106 <victorgabriel101106+github@gmail.com>
2022-11-09 18:30:50 -0300

    feat(game): players

--- resources/public/scripts/game.js
+++ resources/public/scripts/game.js
@@ -1,4 +1,5 @@
 import { Emitter } from "./emitter.js"
+import { Player } from "./player.js"
 
 export function Game() {
 
@@ -11,6 +12,52 @@ export function Game() {
         turn: undefined,
     }
 
+    function addPlayer(player) {
+
+        const isObjectPlayer = player instanceof Player
+        const hasLessTwoPlayers = state.players.length < 2
+        
+        if (isObjectPlayer && hasLessTwoPlayers) {
+            state.players.push(player)
+            emitter.emit('add-player', player)
+        }
+    }
+
+    function getPlayerByTurn() {
+        return state.players[state.turn]
+    }
+
+    function randomTurn() {
+        const playerCount = state.players.length
+        state.turn = Math.floor(Math.random() * playerCount)
+    }
+
+    function toggleTurn() {
+        
+        const playerCount = state.players.length - 1
+        const turnNumber = state.turn || -1
+
+        if (playerCount == turnNumber) {
+            state.turn = 0
+            return
+        }
+
+        state.turn = state.turn + 1
+    }
+
+    function setWinner(player) {
+
+        const playerExists = state.players.find(
+            playerFound => Object.is(player, playerFound)
+        )
+
+        if (playerExists) {
+            state.winner = player
+            state.turn = -1
+            emitter.emit('set-winner', player)
+        }
+    }
+    
     function setRule(newRule) {
         state.rules = {
             ...state.rules,
@@ -28,6 +75,11 @@ export function Game() {
     }
 
     const returnObject = {
+        getPlayerByTurn,
+        toggleTurn,
+        randomTurn,
+        addPlayer,
+        setWinner,
         setRule,
         getRule,
         listen,
---
+++ resources/public/scripts/player.js
@@ -0,0 +1,7 @@
+export class Player {
+    constructor(id) {
+        this.losses = 0
+        this.gains = 0
+        this.id = id
+    }
+}
