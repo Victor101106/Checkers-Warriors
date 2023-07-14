6cb3c2e01b0c0b9c795c0441e47255c0dc766144
Victor101106 <victorgabriel101106+github@gmail.com>
2023-07-14 11:57:17 -0300

    feat: create brazilian board use case

---
+++ source/usecases/create-board-usecase.spec.ts
@@ -0,0 +1,5 @@
+import { describe, test } from "vitest"
+
+describe('Create board use case', () => {
+    test.todo('No implementation')
+})
---
+++ source/usecases/create-board-usecase.ts
@@ -0,0 +1,9 @@
+import { InvalidPosition } from "../domain/board/errors/invalid-position"
+import { InvalidMeasure } from "../domain/board/errors/invalid-measure"
+import { InvalidRange } from "../domain/board/errors/invalid-range"
+import { Board } from "../domain/board/board"
+import { Either } from "../shared/either"
+
+export interface CreateBoardUseCase {
+    execute(): Either<InvalidMeasure | InvalidRange | InvalidPosition, Board>
+}
