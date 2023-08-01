f868aace1f914dab4a2e0aa76168b77eebb36a56
Victor101106 <victorgabriel101106+github@gmail.com>
2023-08-01 16:08:12 -0300

    fix(unauthorized http helper): remove unauthorized access token cookie

--- source/adapters/controllers/helpers/http-helper.ts
+++ source/adapters/controllers/helpers/http-helper.ts
@@ -1,8 +1,9 @@
 import { HttpResponseHeader } from "../ports/http-headers"
 import { HttpResponse } from "../ports/http-response"
+import { serializeCookie } from "./cookie-helper"
 
-export const unauthorized = (error: Error, headers?: Array<HttpResponseHeader>): HttpResponse => ({
-    headers: headers,
+export const unauthorized = (error: Error, headers: Array<HttpResponseHeader> = new Array<HttpResponseHeader>()): HttpResponse => ({
+    headers: [...headers, { name: 'Set-Cookie', value: serializeCookie('access-token', '', { expires: new Date('Thu, 01 Jan 1970 00:00:00 GMT') }) } ],
     code: 401,
     body: { 
         message: error.message, 
