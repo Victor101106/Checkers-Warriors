57ac6ba02758a59cff8c6c316395844fa099b9a6
Victor101106 <victorgabriel101106+github@gmail.com>
2023-08-05 17:12:04 -0300

    chore: blank match page

---
+++ source/main/routes/match-page-route.ts
@@ -0,0 +1,5 @@
+import { FastifyInstance } from "fastify"
+
+module.exports = (instance: FastifyInstance) => instance.get('/match/:id', (request, reply) => {
+    reply.view('match-page.html', { matchId: (request.params as any).id })
+})
