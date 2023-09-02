ae2381272418102611d397cc3a50501368042ba5
Victor101106 <victorgabriel101106+github@gmail.com>
2023-09-02 12:18:42 -0300

    feat: give up use case

---
+++ source/usecases/factory/give-up-usecase-factory.ts
@@ -0,0 +1,4 @@
+import { inMemoryMatchRepository } from "../../external/repositories/factory/match-repository-factory"
+import { GiveUpUseCase } from "../give-up-usecase"
+
+export const giveUpUseCase = new GiveUpUseCase(inMemoryMatchRepository)
