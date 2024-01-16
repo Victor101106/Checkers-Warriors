826a2574e1bce0ee1982c4e1530e455dd13f154d
Victor101106 <victorgabriel101106+github@gmail.com>
2024-01-16 11:03:12 -0300

    fix: rename folder from middlwares to middlewares

--- source/main/factories/application/middlewares/ensure-authenticated-middleware-factory.ts
+++ source/main/factories/application/middlewares/ensure-authenticated-middleware-factory.ts
@@ -1,4 +1,4 @@
-import { EnsureAuthenticatedMiddleware } from "../../../../application/middlwares/ensure-authenticated-middleware"
+import { EnsureAuthenticatedMiddleware } from "../../../../application/middlewares/ensure-authenticated-middleware"
 import { getUserByAccessTokenUseCase } from "../../domain/usecases/get-user-by-access-token-usecase-factory"
 
 export const ensureAuthenticatedMiddleware = new EnsureAuthenticatedMiddleware(getUserByAccessTokenUseCase)
--- source/main/factories/application/middlewares/remains-authenticated-middleware-factory.ts
+++ source/main/factories/application/middlewares/remains-authenticated-middleware-factory.ts
@@ -1,4 +1,4 @@
-import { RemainsAuthenticatedMiddleware } from "../../../../application/middlwares/remains-authenticated-middleware"
+import { RemainsAuthenticatedMiddleware } from "../../../../application/middlewares/remains-authenticated-middleware"
 import { getUserByAccessTokenUseCase } from "../../domain/usecases/get-user-by-access-token-usecase-factory"
 
 export const remainsAuthenticatedMiddleware = new RemainsAuthenticatedMiddleware(getUserByAccessTokenUseCase)
