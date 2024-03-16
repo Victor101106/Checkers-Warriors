d570a4018d71f91c1ac471e38b49e74a51fa6c09
Victor101106 <victorgabriel101106+github@gmail.com>
2024-03-16 10:21:08 -0300

    refactor: move page routes to pages folder

--- source/main/configs/routest.ts
+++ source/main/configs/routest.ts
@@ -6,4 +6,7 @@ export default (instance: FastifyInstance): void => {
     readdirSync(join(__dirname, '../routes/')).forEach(file => {
         require(join(__dirname, `../routes/${file}`))(instance)
     })
+    readdirSync(join(__dirname, '../pages/')).forEach(file => {
+        require(join(__dirname, `../pages/${file}`))(instance)
+    })
 }
